66860f9f68c228ef687cbf25b27ea05fab7cc360
MohammadAli Saeidi <1saeidima@gmail.com>
2023-08-03 17:15:27 +0330

    feat: add inputs for login form doc: remove previous readme.md content

--- src/pages/AuthenticationPage/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage/AuthenticationPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './AuthenticationPage.css'
+import {Button, Divider, Paper, Stack, TextField} from "@mui/material";
 
 function AuthenticationPage() {
-	return (
-		<div className='auth-page'>
-			<div className='form-container'>
-				this is my form
-			</div>
-		</div>
-	);
+    return (
+        <div className='auth-page'>
+            <Paper className='form-paper' elevation={4}>
+                <div className='form-container'>
+                    <Stack>
+                        <TextField id='username' margin='dense' label='username'/>
+                        <TextField id='password' margin='dense' label='password'/>
+                        <Divider  variant='middle' />
+                        <Button variant="contained">Contained</Button>
+                    </Stack>
+                </div>
+            </Paper>
+        </div>
+    );
 }
 
 export default AuthenticationPage;
